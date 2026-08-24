f8342d11dcbc185a106b9b20845df40edd769a20
Ariana Molina <ariana.molina.c@outlook.com>
2026-08-24 16:28:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -108,4 +108,4 @@ async def extract_pdf(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8080)
